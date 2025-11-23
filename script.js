2c5990915e0ac4e880cc8a9b34c2a071418f6922
Tayansam <samiraramo815@gmail.com>
2025-11-23 16:50:31 +0300

    html,css

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ navLinks.querySelectorAll('a').forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+/*-----------------Contact-Form-----------------*/
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbxdA6bSYEXA_P6u8RPA4c2hZvuPPf07vWrYyQrSLqv3lzCOj6BRJJKeaV_Z_zMdIKlQYQ/exec";
+
+const form = document.getElementById("contactform");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  msg.innerHTML = "Sending message...";
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Server returned ${response.status}`); // ✅ fixed
+      }
+
+      msg.innerHTML = "✅ Message sent successfully!";
+      form.reset();
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 3000);
+    })
+    .catch((error) => {
+      console.error("Form submit error:", error);
+      msg.innerHTML = "❌ Failed to send message. Try again later.";
+    });
+});
